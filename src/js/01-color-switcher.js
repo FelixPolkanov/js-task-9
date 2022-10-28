a7a917455b65e1f297acd0c201724b7db4726ded
FelixPolkanov <felixpolkanov@gmail.com>
2022-10-28 12:40:10 +0300

    update hw_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyBgc = document.querySelector('body');
+let setInt;
+
+btnStart.addEventListener('click', onStartClick)
+btnStop.addEventListener('click', onStopClick)
+
+
+ function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+
+function onStartClick() {
+  btnStart.setAttribute('disabled', true) 
+      setInt = setInterval(() => {
+      bodyBgc.style.backgroundColor = getRandomHexColor();
+      console.log('backgroundColor: ', getRandomHexColor())
+  }, 1000)
+ }
+
+
+function onStopClick() { 
+  clearInterval(setInt)
+  btnStart.removeAttribute('disabled')
+  console.log('off')
+}
+
+
+
+
+
+
